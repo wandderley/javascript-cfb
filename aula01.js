abdfee086937ff25f1529aa0f4e9633d97ba9b2c
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-03-25 23:26:38 -0300

    aula01

--- aula01.js
+++ aula01.js
@@ -1,1 +1,7 @@
-console.log("")
+//console.log("Esta linha está comentada")
+
+/* Varias linhas comentadas
+console.log("Esta linha está comentada")
+console.log("Alô Mundo")
+*/
+console.log("Fim")
