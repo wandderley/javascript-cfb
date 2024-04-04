a7cc0b730653f4159493ec3da4c1d1298a260cc4
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-04-03 23:46:04 -0300

    aula03

---
+++ aula003/aula03.js
@@ -0,0 +1,27 @@
+"use strict"
+
+let nome = 'Luiz'
+
+nome = "CFB Cursos"
+nome = 10
+
+const cursos = "JavaScript"
+
+//curso = "HTML"
+
+console.log(cursos)
+
+/* let e var
+function teste(){
+   
+    if(true){
+        let nome = 'Luiz'
+        console.log("dentro do IF do teste: " + nome)
+    }
+    console.log("dentro de teste: " + nome)
+}
+
+teste()
+
+console.log("fora de teste: " + nome)
+*/
