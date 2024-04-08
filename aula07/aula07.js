6a5c0aedd6e359747bc9c80194fb4088ac11257d
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-04-08 18:33:46 -0300

    aula07 - operadores lógicos

---
+++ aula07/aula07.js
@@ -0,0 +1,19 @@
+// tabela verdade
+// && -> and e
+// || -> or ou
+// ! -> not não
+
+let n1,n2,n3,n4
+n1=10
+n2=5
+n3=15
+n4=2
+
+if (!(n1>n2) && n3>n4) {
+    console.log("verdadeiro")
+}else{
+    console.log("falso")
+}
+
+//console.log(!((n1>n2) || (n1>n3)))
+
