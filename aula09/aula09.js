76bcc1f28c9efb1722a6e60d0239f27a33025aa0
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-04-11 17:44:34 -0300

    aula09 - pré incremento e pós incremento

---
+++ aula09/aula09.js
@@ -0,0 +1,11 @@
+let n=10
+let n1 = 10
+let n2 = 20
+
+console.log(n1 + "" +  n2) // o mais(+) passa apenas a concatenar e não a somar os valores
+
+// x =-10 // o - é um operador de inversão ou negação
+//console.log(n++) // pós incremento
+//console.log(++n) // pré incremento
+
+//console.log(n)
