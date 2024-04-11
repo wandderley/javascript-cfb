9afc309d17983d40a96d8d650d4a1080fdf7fe14
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-04-10 23:31:36 -0300

    aula08 - bitwise

---
+++ aula08/aula08.js
@@ -0,0 +1,12 @@
+// Bitwise
+// & diferentes 0 iguais 1 - and
+// | se houver 1 será 1 - or
+// ^ onde não há equivalencia 1 -  xor
+// << n  deloca n bits(s) para a esquerda - aumenta o valor, cada bit dobra
+// >> n  desloca n bits(s) para a diireita - diminui o valor, cada bit divide 
+let n1=10
+let n2=11
+
+let res = n1 << 1
+
+console.log(res)
