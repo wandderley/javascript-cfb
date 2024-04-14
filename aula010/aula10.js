5ef78cf71dd3dda12e907579d4ed303407a846d4
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-04-14 11:27:36 -0300

    aula10 - operador ternário

---
+++ aula010/aula10.js
@@ -0,0 +1,28 @@
+// Operações ternárias
+
+let st ="A"
+
+res = (st == "A" ? "Ativo" : "Inativo")
+console.log(res)
+
+/* Outro exemplo de operador ternário
+let num1 = 10
+let num2 = 5
+res = (num1 > num2 ? "Verdadeiro" : "Falso")
+console.log(res)
+*/
+
+/* Condição usando operador ternário
+let num = 10
+res = (!(num%2) ? "Par" : "Impar") //(condição) ? se verdadeiro : se falso
+console.log(res)
+*/
+
+/* Condição completa
+let res = num%2
+if(res == 0){
+    console.log("par")
+} else{
+    console.log("impar")
+}
+*/
