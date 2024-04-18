6323f406a6b537f0992b034752dbfa97e26a60a0
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-04-18 17:44:22 -0300

    aula13 - comando condicional if e if else

---
+++ aula013/aula13.js
@@ -0,0 +1,28 @@
+// comando condicional if e if else
+
+let clima = "chuvendo"
+let energia = 100
+
+if (energia > 70 && clima =="sol") {
+    console.log("vou a praia")
+}else {
+    console.log("vou ao cinema")
+}
+
+console.log("fim do programa")
+
+
+/*
+let num = 30
+if(num > 10){
+    console.log("numeral maior que 10")
+    if(num > 50){
+        console.log("Numeral maior que 50")
+    }
+}else if (num > 5){
+    console.log("numeral esta entre 6 e 10")
+}else {
+    console.log("numeral menor ou igual a 5")
+}
+console.log("fim do programa")
+*/
