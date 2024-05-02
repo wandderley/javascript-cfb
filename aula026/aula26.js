006a24182055a84207e21b8525103eb3cf9762bf
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-05-02 13:51:16 -0300

    aula26 - funções dentro de funções

---
+++ aula026/aula26.js
@@ -0,0 +1,12 @@
+const somar = val => {
+    let res = 0
+    for (v of val)
+        res+=v
+    return res
+}
+
+const soma = (...valores)=>{    
+    return somar(valores)
+}
+
+console.log(soma(10,5,2))
