8049b9eedbf09519e0d812f2c3b2d24d4fa7a1cd
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-06-01 12:37:13 -0300

    aula41 - removendo elementos do DOM com removeChild

---
+++ aula041/aula41.js
@@ -0,0 +1,31 @@
+const caixa1 = document.querySelector("#caixa1")
+const btn_c = [...document.querySelectorAll(".curso")]
+const c1_2 = document.querySelector("#c1_2")
+const cursos = ["HTML", "CSS", "JavaScript", "PHP", "React", "MySQL", "ReactNative"]
+
+cursos.map((elemento, chave) => {
+    console.log(chave)
+    const novoElemento = document.createElement("div") // criando um elemento div
+    novoElemento.innerHTML = elemento // adicionando texto
+    novoElemento.setAttribute("id", "c" + (chave + 1)) // adicionando id
+    novoElemento.setAttribute("class", "curso c1") // adicionando classe
+    const btn_lixeira = document.createElement("img")
+    btn_lixeira.setAttribute("src", "img/trash.png")
+    btn_lixeira.setAttribute("class", "btn_lixeira")
+    btn_lixeira.addEventListener("click", (evento) => {
+        console.log(evento.target.parentNode)
+        caixa1.removeChild(evento.target.parentNode)
+    })
+    novoElemento.appendChild(btn_lixeira)
+
+    caixa1.appendChild(novoElemento) // adiciona o elemento à caixa1
+})
+
+
+/* Criação de elemento e adicionar atributos à ele 
+    const novoElemento = document.createElement("div") // criando um elemento div
+    novoElemento.innerHTML = "ReactNative" // adicionando texto
+    novoElemento.setAttribute("id","c7") // adicionando id
+    novoElemento.setAttribute("class","curso c1") // adicionando classe
+    caixa1.appendChild(novoElemento)// adiciona o elemento à caixa1 
+*/
