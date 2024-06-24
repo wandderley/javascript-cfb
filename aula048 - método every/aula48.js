93bc2ea9fbaadc32813f7c4d6733ef038cd9d741
Wandderley Luiz <160080178+wandderley@users.noreply.github.com>
2024-06-24 19:18:30 -0300

    aula048 - método every

---
+++ aula048 - método every/aula48.js
@@ -0,0 +1,20 @@
+const p_array = document.querySelector("#array")
+const btnVerificar = document.querySelector("#btnVerificar")
+const resultado = document.querySelector("#resultado")
+
+const elementos_array = [21, 25, 19, 20, 16, 18, 22]
+
+p_array.innerHTML = "[" + elementos_array + "]"
+
+btnVerificar.addEventListener("click", (evt) => {
+    const retorno = elementos_array.every((e, i) => {
+        if (e < 18) {
+            resultado.innerHTML = "Array conforme na posição " + i
+        }
+        return e >= 18
+    })
+    if(retorno){
+        resultado.innerHTML = "Array conforme"
+    }
+    console.log(retorno)
+})
