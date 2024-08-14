b85ac401a6c203c9581bec748cc1ee050f706049
wandderley luiz <wandderley.luiz@hotmail.com>
2024-08-14 20:41:46 -0300

    aula061 - objetos05 desafio

--- aula061 - objetos05/aula61.js
+++ aula061 - objetos05/aula61.js
@@ -18,14 +18,32 @@ const Pessoa = {
     }
 }
 
-const p2 = Pessoa
-const p3 = Pessoa
+//             DESAFIO
+const nome = document.querySelector("#f_nome")
+const idade = document.querySelector("#f_idade")
+const add = document.querySelector("#btn_add")
+const res = document.querySelector(".res")
 
-// alterando a propriedade "nome"
-p3.nome = "Cladisvardon"
-p2["nome"] = "Bridgertrudsmelda"
-Pessoa.setNome("Patricscrerdison")
+add.addEventListener("click", () => {
+    Pessoa.setNome(nome.value)
+    Pessoa.setIdade(idade.value)
 
-console.log(Pessoa.nome)
-console.log(p2.getNome())
-console.log(p3.nome)
+    let div = document.createElement("div")
+    div.setAttribute("class", "pessoa")
+    div.innerHTML = (`Nome: ${Pessoa.getNome()}<br/> Idade: ${Pessoa.getIdade()}`)
+
+    res.appendChild(div)
+})
+
+//                 CONTEUDO DA AULA
+// const p2 = Pessoa
+// const p3 = Pessoa
+
+// // alterando a propriedade "nome"
+// p3.nome = "Cladisvardon"
+// p2["nome"] = "Bridgertrudsmelda"
+// Pessoa.setNome("Patricscrerdison")
+
+// console.log(Pessoa.nome)
+// console.log(p2.getNome())
+// console.log(p3.nome)
