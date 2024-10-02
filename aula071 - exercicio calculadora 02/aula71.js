c40d8a113693501a4be8a6f6b6cc136af1159442
wandderley luiz <wandderley.luiz@hotmail.com>
2024-10-02 20:29:58 -0300

    aula071 - exercicio calculadora 02

---
+++ aula071 - exercicio calculadora 02/aula71.js
@@ -0,0 +1,26 @@
+const teclasNum = [...document.querySelectorAll(".num")]
+const teclasOp = [...document.querySelectorAll(".op")]
+const teclaRes = document.querySelectorAll(".res")
+const display = document.querySelector(".display")
+const ton = document.getElementById("ton")
+const tlimpar = document.getElementById("tlimpar")
+
+teclasNum.forEach((el) =>{
+    el.addEventListener("click", (evt)=>{
+        display.innerHTML += evt.target.innerHTML
+    })
+})
+
+teclasOp.forEach((el) =>{
+    el.addEventListener("click", (evt)=>{
+        display.innerHTML += evt.target.innerHTML
+    })
+})
+
+tlimpar.addEventListener("click", (evt)=>{
+    display.innerHTML = "0"
+})
+
+console.log(teclasNum)
+console.log(teclasOp)
+console.log(teclaRes)
