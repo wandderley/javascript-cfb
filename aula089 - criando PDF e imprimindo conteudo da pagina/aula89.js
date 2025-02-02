1ffa307fe1b0a934b980d1c657732883f2ea692c
wandderley luiz <wandderley.luiz@hotmail.com>
2025-02-02 20:26:59 -0300

    aula089 - criando PDF e imprimindo conteudo da pagina

---
+++ aula089 - criando PDF e imprimindo conteudo da pagina/aula89.js
@@ -0,0 +1,24 @@
+const btn_imp = document.getElementById("btn_imp")
+
+btn_imp.addEventListener("click", (evt)=>{
+    const conteudo = document.getElementById("tabela").innerHTML;
+    let estilo = "<style>";
+    estilo += "table {width: 100%; font: 25px Calibri;}";
+    estilo += "table, td, th {border: solid 2px #888; border-collapse: collapse;}";
+    estilo += "padding: 4px 8px; text-align: center;"
+    estilo += "</style>"
+
+    const win = window.open('', '', 'height=700, width=700')
+    win.document.write('<html><head>')
+    win.document.write('<title>CFB Cursos</title>')
+    win.document.write(estilo)
+    win.document.write('</head>')
+    win.document.write('<body>')
+    win.document.write(conteudo)
+    win.document.write('</body></html>')
+
+    win.document.close();
+
+    win.print();
+    // window.print()
+})
